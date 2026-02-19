2233d7061b062fd37c7f11e58ee7d3b8bbfe24ac
rileyyy <rileyaari@gmail.com>
2026-02-19 15:23:05 -0500

    Player Character Controller & Script

---
+++ WalkingSim_GameCoding2/Assets/Scripts/CCPlayer.cs
@@ -0,0 +1,153 @@
+using Unity.VisualScripting;
+using UnityEngine;
+using UnityEngine.InputSystem;
+using UnityEngine.UI;
+
+public class CCPlayer : MonoBehaviour
+{
+    [Header("Movement")]
+    public float walkSpeed = 5f;
+    public float sprintSpeed = 9f;
+    public float jumpHeight = 5f;
+
+    public Transform cameraTransform;
+    public float lookSensitivity = 1f;
+
+    private CharacterController cc;
+
+    private Vector2 moveInput;
+    private Vector2 lookInput;
+
+    private float verticalVelocity; //current upward/downward speed when we jump
+    private float gravity = -20f; //constant downward acceleration
+
+    private float pitch; //up & down
+
+    //interaction variables
+    private GameObject currentTarget;
+    public Image reticleImage;
+    private bool interactPressed;
+
+    private bool isSprinting;
+    private bool isJumping;
+
+    private void Awake()
+    {
+        cc = GetComponent<CharacterController>();
+
+        //optional cursor locking
+        Cursor.lockState = CursorLockMode.Locked;
+        Cursor.visible = false;
+
+        //find reticle
+        reticleImage = GameObject.Find("Reticle").GetComponent<Image>();
+        reticleImage.color = new Color(0, 0, 0, 0.7f); //slightly transparent black
+    }
+
+    void Update()
+    {
+
+    }
+    void Start()
+    {
+
+    }
+
+    private void HandleLook()
+    {
+        //horizontal movement rotates player
+        float yaw = lookInput.x * lookSensitivity;
+        //vertical mouse movement rotates camera
+        float pitchDelta = lookInput.y * lookSensitivity;
+        
+        transform.Rotate(Vector3.up * yaw);
+
+        //accumulate vertical rotation
+        pitch -= pitchDelta;
+        //clamp it so we dont flip upside down
+        pitch = Mathf.Clamp(pitch, -90, 90);
+
+        cameraTransform.localRotation = Quaternion.Euler(pitch, 0, 0);
+    }
+    private void HandleMovement()
+    {
+        //updating our bool to be true or false if the player is grounded
+        bool grounded = cc.isGrounded;
+        Debug.Log("is grounded: " + grounded);
+
+        //this keeps the character controller snapped to the ground
+        if(grounded && verticalVelocity <= 0)
+        {
+            verticalVelocity = -2f;
+        }
+
+        float currentSpeed = walkSpeed;
+
+        //if running is true, set current speed to runSpeed
+        if(isSprinting)
+        {
+            currentSpeed = sprintSpeed;
+        } //if it is false set it back to walkSpeed
+        else if (!isSprinting)
+        {
+            currentSpeed = walkSpeed;
+        }
+
+        Vector3 move = transform.right * moveInput.x * currentSpeed + transform.forward * moveInput.y * currentSpeed;
+
+        //if jumping is true & we are grounded
+        if(isJumping && grounded)
+        {
+            verticalVelocity = Mathf.Sqrt(jumpHeight * -2f * gravity);
+        }
+        else
+        {
+            isJumping = false;
+        }
+
+        //apply gravity to every frame
+        verticalVelocity += gravity * Time.deltaTime;
+        //convert verticalVelocity into movement vector
+        Vector3 velocity = Vector3.up * verticalVelocity;
+        //now we are finally MOVING our PLAYER
+
+        cc.Move((move + velocity) * Time.deltaTime);
+
+    }
+
+    public void OnMove(InputAction.CallbackContext context)
+    {
+        moveInput = context.ReadValue<Vector2>();
+    }
+
+    public void OnLook(InputAction.CallbackContext context)
+    {
+        lookInput = context.ReadValue<Vector2>();
+    }
+
+    public void OnJump(InputAction.CallbackContext context)
+    {
+        //if we are actually hitting the key, isJumping = true
+        if(context.performed)
+        {
+            isJumping = true;
+        }
+    }
+
+    public void OnSprint(InputAction.CallbackContext context)
+    {
+        isSprinting = context.ReadValueAsButton();
+
+    }
+    public void OnInteract(InputAction.CallbackContext context)
+    {
+        if (context.performed) interactPressed = true;
+    }
+
+    //example
+    private void OnControllerColliderHit(ControllerColliderHit hit)
+    {
+        Debug.Log("CC collided with: " + hit.gameObject.name);
+    }
+    
+}
