7216b3fd3c0c52877ad34ca912dde705d9be1a03
rileyyy <rarico@pratt.edu>
2026-02-26 15:27:01 -0500

    Set up dialogue manager script & Empty

--- WalkingSim_GameCoding2/Assets/Scripts/CCPlayer.cs
+++ WalkingSim_GameCoding2/Assets/Scripts/CCPlayer.cs
@@ -29,6 +29,7 @@ public class CCPlayer : MonoBehaviour
     public Image reticleImage;
     private bool interactPressed;
     public static event Action<NPCData> OnDialogueRequested;
+    private Interactable currentInteractable;
 
     private bool isSprinting;
     private bool isJumping;
@@ -127,19 +128,24 @@ public class CCPlayer : MonoBehaviour
         //make a ray that goes straight out of the camera(center of screen)
         //players eyesight
         Ray ray = new Ray(cameraTransform.position, cameraTransform.forward);
-        RaycastHit hit;
+        //RaycastHit hit;
         //asking unity if it hit something within 3 units
         //hit stores what we hit like the collider
-        bool didHit = Physics.Raycast(ray, out hit, 3);
-        if (!didHit) return;//if we didn't hit anything start here
+        //bool didHit = Physics.Raycast(ray, out hit, 3);
+        //if (!didHit) return;//if we didn't hit anything start here
         //if we hit something tagged interactable
-        if (hit.collider.CompareTag("Interactable"))
+        if (Physics.Raycast(ray, out RaycastHit hit, 3f))
         {
-            //store the object so we can destroy or do whatever when the player clicks
-            currentTarget = hit.collider.gameObject;
-            if (reticleImage != null)
+
+            currentInteractable = hit.collider.GetComponentInParent<Interactable>();
+            if(currentInteractable != null && reticleImage != null)
             {
                 reticleImage.color = Color.red;
+                Debug.DrawRay(cameraTransform.position, cameraTransform.forward * 3, Color.blue);
+            }
+            else
+            {
+                Debug.DrawRay(cameraTransform.position, cameraTransform.forward * 3, Color.blue);
             }
         }
 
@@ -153,11 +159,11 @@ public class CCPlayer : MonoBehaviour
         //consume the input so one click only triggers one interactions
         //this changes next frame
         interactPressed = false;
-        if (currentTarget == null) return;
-        Destroy(currentTarget);
-        //clear target reference after destroying
-        currentTarget = null;
-
+        if(currentInteractable == null)
+        {
+            return;
+        }
+        currentInteractable.Interact(this);
     }
 
     public void OnMove(InputAction.CallbackContext context)
---
+++ WalkingSim_GameCoding2/Assets/Scripts/NPCInteractable.cs
@@ -0,0 +1,18 @@
+using Unity.Android.Gradle.Manifest;
+using UnityEngine;
+
+public class NPCInteractable : Interactable
+{
+    public NPCData npcData;
+
+    public override void Interact(CCPlayer ccplayer)
+    {
+        if(npcData == null)
+        {
+            Debug.Log("NPC has no data: " + gameObject.name);
+        }
+
+        //if we are interacting with the NPC and it has data, then request dialogue
+        ccplayer.RequestDialogue(npcData);
+    }
+}
